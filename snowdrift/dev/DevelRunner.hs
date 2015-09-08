ffae467741b2e740ee0f74e9baf27d95b9ca7e57
Bryan Richter <b@chreekat.net>
2015-09-08 13:23:08 -0700

    Remove vestigial subproject, promoting ghci-runner

--- snowdrift/dev/DevelRunner.hs
+++
@@ -1,8 +0,0 @@
-module DevelRunner (main, shutdown) where
-
-import qualified Snowdrift
-import Yesod
-import qualified Dev.Runner as Dev
-
-main = Dev.update (warp 3000 Snowdrift.Site) (return ())
-shutdown = Dev.shutdown
--- snowdrift/test/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Snowdrift
-import System.Exit
-
-main = do
-    putStrLn "Wuttt"
-    exitSuccess
